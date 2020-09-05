b59ed0d6741474bcc667043375c80ca00dfee4de
Ali Haider <alihaiderx736@gmail.com>
2020-09-05 22:42:23 +0100

    3-dog-photo-gallery completed

---
+++ week-2/Homework/mandatory/3-dog-photo-gallery/exercise.js
@@ -0,0 +1,27 @@
+
+var source;
+
+//api fetch
+let getButton = document.querySelector("button");
+getButton.addEventListener("click", function () {
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(function (response) {
+    return response.json();
+})
+
+
+ 
+.then(function (data) {
+        let newImage = document.createElement("img");
+        newImage.src = data.message;
+        newImage.style.width = "150px";
+        newImage.style.height = "150px";
+        let ul = document.querySelector("ul");
+        ul.append(newImage);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+
