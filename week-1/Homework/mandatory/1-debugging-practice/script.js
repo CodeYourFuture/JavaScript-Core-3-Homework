ba253c70a3251bb4ff626a0d0e68023807b241bb
Leroy Douglas <leroi.douglas@gmail.com>
2020-09-05 21:50:18 +0100

    Update script.js
    Fixed syntax & logic errors

--- week-1/Homework/mandatory/1-debugging-practice/script.js
+++ week-1/Homework/mandatory/1-debugging-practice/script.js
@@ -25,20 +25,34 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const check = document.getElementById("check");
 
+function bookExists(author, title){
+  for (let bookID = 0; bookID < myLibrary.length; ++bookID){
+    if (myLibrary[bookID].title === title && myLibrary[bookID].author === author) {
+      return true;
+    }
+  }
+  return false;
+}
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
   if (
     title.value == null ||
     title.value == "" ||
+    author.value == null ||
+    author.value == ""  ||
     pages.value == null ||
     pages.value == ""
   ) {
     alert("Please fill all fields!");
-    return false;
-  } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    return false; 
+  } else if(bookExists(author.value, title.value)){
+      alert("Cannot add this book as it already exists in the library");
+      return false;
+  }
+   else {
+    let book = new Book(title.value, author.value, pages.value, check.checked);
+    myLibrary.push(book);
     render();
   }
 }
@@ -54,7 +68,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
@@ -77,9 +91,9 @@ function render() {
     cell4.appendChild(changeBut);
     let readStatus = "";
     if (myLibrary[i].check == false) {
-      readStatus = "Yes";
-    } else {
       readStatus = "No";
+    } else {
+      readStatus = "Yes";
     }
     changeBut.innerHTML = readStatus;
 
@@ -90,11 +104,11 @@ function render() {
 
     //add delete button to every row and render again
     let delButton = document.createElement("button");
-    delBut.id = i + 5;
-    cell5.appendChild(delBut);
-    delBut.className = "btn btn-warning";
-    delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    delButton.id = i + 5;
+    cell5.appendChild(delButton);
+    delButton.className = "btn btn-warning";
+    delButton.innerHTML = "Delete";
+    delButton.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
