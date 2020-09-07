a412f52964259838684751c98c2943894799812b
Sadat <sadat0akhtar@gmail.com>
2020-09-07 13:11:51 +0100

    completed dog gallery exercise with fetch api

---
+++ week-2/Homework/mandatory/3-dog-photo-gallery/script.js
@@ -0,0 +1,26 @@
+//SELECTORS
+let btnOne = document.getElementById('buttonOne');
+let btnTwo = document.getElementById('buttonTwo');
+let dogImage = document.getElementById('dogImage');
+
+//EVENT LISTENERS
+btnOne.addEventListener('click', () => {
+    getDogImages();
+});
+btnTwo.addEventListener('click', getDogImages);
+
+//FUNCTION TO GET DOG PICTURES(API)
+function getDogImages(){
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        //console.log(data.message);
+        document.getElementById('dogImage').src = data.message;
+    })
+    .catch(err => {
+        console.log('error!!!!');
+        console.error(err);
+    });
+};
