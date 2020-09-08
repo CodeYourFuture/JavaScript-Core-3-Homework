6188e88b26098ba13d2cacc36747745cdf8477c0
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-09-09 00:53:08 +0100

    Dog Gallery completed

---
+++ week-2/Homework/mandatory/3-dog-photo-gallery/dogGallery.js
@@ -0,0 +1,20 @@
+var btn = document.getElementById('btn');
+var frame = document.getElementById('frame');
+const URL = 'https://dog.ceo/api/breeds/image/random';
+
+btn.addEventListener('click',function(){
+    //when the button is clicked getData() function is triggered
+    getData()
+});
+
+async function getData() {
+
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    var img = document.createElement("img");
+    img.setAttribute('src', data.message);
+    img.setAttribute('width', '300px');
+    img.setAttribute('height', '300px');
+    frame.appendChild(img);
+}
