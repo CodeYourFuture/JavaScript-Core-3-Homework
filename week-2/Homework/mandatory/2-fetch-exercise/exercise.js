91b046d8ea61e450e94ff15269ef8a2fc09717b1
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2020-09-09 04:02:06 +0100

    Exercise 2 & 3 completed

--- week-2/Homework/mandatory/2-fetch-exercise/exercise.js
+++ week-2/Homework/mandatory/2-fetch-exercise/exercise.js
@@ -16,11 +16,18 @@ Expected result
 Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
+let greeting = document.querySelector("#greeting-text");
 
-fetch('*** Write the API address here ***')
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(greeting) {
-        // Write the code to display the greeting text here
-    });
+fetch("https://codeyourfuture.herokuapp.com/api/greetings")
+  .then((response) => response.text())
+  .then((data) => {
+    greeting.textContent = data;
+  });
+
+// fetch('*** Write the API address here ***')
+//     .then(function(response) {
+//         return response.text();
+//     })
+//     .then(function(greeting) {
+//         // Write the code to display the greeting text here
+//     });
---
+++ week-2/Homework/mandatory/3-dog-photo-gallery/script.js
@@ -0,0 +1,18 @@
+let imageDisplay = document.querySelector(".displayImage");
+let displayBtn = document.querySelector("#displayBtn");
+let displayImage = document.querySelector(".displayImage");
+let imageEl = document.createElement("img");
+let liEl = document.createElement("li");
+displayBtn.addEventListener("click", uploadRandomDogImage);
+
+function uploadRandomDogImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      imageEl.src = data.message;
+      imageEl.className = "dogImage";
+      liEl.appendChild(imageEl);
+      displayImage.appendChild(liEl);
+    })
+    .catch((err) => console.log(err));
+}
