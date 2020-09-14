11c43320ffd1314914df7126e17117f669746546
Hiba-moh <Hiba.moh@yahoo.com>
2020-09-14 23:19:33 +0100

    Mandatory-1-Practice-app.js- Done

--- week-3/Homework/mandatory/1-practice/app.js
+++ week-3/Homework/mandatory/1-practice/app.js
@@ -4,6 +4,9 @@ let lat;
 let temperatureDescription = document.querySelector('.temperature-description');
 let temperatureDegree = document.querySelector('.temperature-degree');
 let locationTimeZone = document.querySelector('.location-timezone');
+let temperatureSection = document.querySelector('.temperature');
+const temperatureSpan = document.querySelector('.temperature span');
+
 if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(position =>{
     long=position.coords.longitude;
@@ -23,9 +26,22 @@ if(navigator.geolocation){
 temperatureDegree.textContent = temperature;
 temperatureDescription.textContent = summary;
 locationTimeZone.textContent = data.timezone;
+//Formula For CELSIUS
+let celsius = (temperature - 32) * (5 / 9);
 //set Icon
 setIcons(icon,document.querySelector('.icon'));
-  });
+  
+//change teperature to celsius/farenheit
+temperatureSection.addEventListener('click',()=>{
+  if(temperatureSpan.textContent === 'F'){
+    temperatureSpan.textContent = 'C';
+    temperatureDegree.textContent = Math.floor(celsius);
+  }else{
+    temperatureSpan.textContent = 'F';
+    temperatureDegree.textContent = temperature;
+  }
+});
+});
 
 });
 
