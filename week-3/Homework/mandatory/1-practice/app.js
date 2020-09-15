7e5af66898863c6df60c51d0804507b1e0f7a063
Gennady Tabala <gtabala@gmail.com>
2020-09-15 20:10:08 +0100

    Done.

--- week-3/Homework/mandatory/1-practice/app.js
+++ week-3/Homework/mandatory/1-practice/app.js
@@ -1,16 +1,52 @@
-window.addEventListener("load", () =>{
-    let long;
-    let lat;
-// alert(navigator.geolocation);
-    if(navigator.geolocation){
-        console.log("Here you are: 1");
-        navigator.geolocation.getCurrentPosition((position) => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            console.log(long, lat);
+window.addEventListener("load", () => {
+  let lon;
+  let lat;
+  let units = "&units=metric";
 
-            
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1912a0abbf3e8fabf3a7532ad1fe6bf3${units}`;
+
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let celsius = data.main.temp;
+          let fahrenheit = ((celsius * 9) / 5 + 32).toFixed(2);
+          document.querySelector(".temperature-degree").innerHTML = celsius;
+          document.querySelector(".temperature-description").innerHTML =
+            data.weather[0].description[0].toUpperCase() +
+            data.weather[0].description.slice(1);
+          document.querySelector(".location-timezone").innerHTML =
+            data.name + " " + data.sys.country;
+          document.querySelector(
+            ".location p"
+          ).innerHTML = `<img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+
+          document
+            .querySelector(".temperature")
+            .addEventListener("click", () => {
+              if (
+                document.querySelector(".degree-section span").innerText === "C"
+              ) {
+                document.querySelector(".degree-section span").innerText = "F";
+                document.querySelector(
+                  ".temperature-degree"
+                ).innerHTML = fahrenheit;
+              } else {
+                document.querySelector(".degree-section span").innerText = "C";
+                document.querySelector(
+                  ".temperature-degree"
+                ).innerHTML = celsius;
+              }
+            });
         })
-    } else {console.log("Ups!")};
+        .catch((err) => console.log(err));
+    });
+  } else {
+    console.log("Ups!");
+  }
 });
-
