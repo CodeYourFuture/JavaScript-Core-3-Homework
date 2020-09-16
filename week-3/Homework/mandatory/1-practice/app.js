5d801dd74887f12eb3f6b5917374c67545efe4e5
Mursel Aysan <aysanmursel@gmail.com>
2020-09-16 13:23:59 +0100

    Code -reading DONE

--- week-3/Homework/mandatory/1-practice/app.js
+++ week-3/Homework/mandatory/1-practice/app.js
@@ -58,29 +58,28 @@ temperatureSection.addEventListener('click',()=>{
 
 
     
-        const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/`;
-    
-      fetch(api)
-      .then(response =>{
-          return response.json();
-      })
-      .then(data =>{
-        console.log(data)
-          const {temperature,summary, icon}= data.currently;
-    //set Dom Elements from the API
-    temperatureDegree.textContent = temperature;
-    temperatureDescription.textContent = summary;
-    locationTimeZone.textContent = data.timezone;
-    //set Icon
-    setIcons(icon,document.querySelector('.icon'));
-      });
-    
-    
-    
-    
-    function setIcons(icon,iconID){
-    const skyCons = new Skycons({color:'white'});
-    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-    skyCons.play();
-    return skyCons.set(iconID,Skycons[currentIcon]);
-    };
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+fetch(api)
+.then(response =>{
+  return response.json();
+})
+.then(data =>{
+console.log(data)
+  const {temperature,summary, icon}= data.currently;
+//set Dom Elements from the API
+temperatureDegree.textContent = temperature;
+temperatureDescription.textContent = summary;
+locationTimeZone.textContent = data.timezone;
+//set Icon
+setIcons(icon,document.querySelector('.icon'));
+});
+
+
+function setIcons(icon,iconID){
+const skycons = new Skycons({color:'white'});
+const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+skycons.play();
+return skycons.set(iconID,Skycons[currentIcon]);
+}
