a4a8320d73d57a2b2c7ab25853310f2a8b9c5410
DenizAri <58032174+DenizAri@users.noreply.github.com>
2020-09-17 09:45:20 +0100

    exercise 1 is done

--- week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
+++ week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
@@ -1,11 +1,13 @@
 const personOne = {
-    name: 'Popeye',
-    age: 34,
-    favouriteFood: 'Spinach'
-}
+  name: "Popeye",
+  age: 34,
+  favouriteFood: "Spinach",
+};
 
-function introduceYourself(___________________________) {
-    console.log (`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`); 
+function introduceYourself({ name, age, favouriteFood }) {
+  console.log(
+    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+  );
 }
 
 introduceYourself(personOne);
