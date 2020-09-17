65e4a752f7d5fb44a0f58401f133ee57a5af0a04
DenizAri <58032174+DenizAri@users.noreply.github.com>
2020-09-17 09:43:43 +0100

    update the file

--- week-3/Homework/mandatory/2-exercises/exercise-2/exercise-2.js
+++ week-3/Homework/mandatory/2-exercises/exercise-2/exercise-2.js
@@ -1,11 +1,78 @@
 let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    house: "Gryffindor",
+    pet: "Owl",
+    occupation: "Student",
+  },
+  {
+    firstName: "Hermione",
+    lastName: "Granger",
+    house: "Gryffindor",
+    pet: "Cat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Cedric",
+    lastName: "Diggory",
+    house: "HufflePuff",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
+
+hogwarts.forEach((student) => {
+  let { house, firstName, lastName } = student;
+  if (house === "Gryffindor") {
+    console.log(firstName, lastName);
+  }
+});
+hogwarts.forEach((person) => {
+  let { firstName, lastName, occupation, pet } = person;
+  if (occupation === "Teacher" && pet !== null) {
+    console.log(firstName, lastName);
+  }
+});
