5461f6668aedee4ec3d8960990fb10a0856f382c
Denis-Andrei <denispeptanariu@gmail.com>
2020-09-18 22:02:56 +0100

    1st exercise DONE

--- week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
+++ week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
@@ -4,7 +4,9 @@ const personOne = {
     favouriteFood: 'Spinach'
 }
 
-function introduceYourself(___________________________) {
+const {name, age, favouriteFood} = personOne; //answer to question 1
+
+function introduceYourself({name, age, favouriteFood}) {
     console.log (`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`); 
 }
 
