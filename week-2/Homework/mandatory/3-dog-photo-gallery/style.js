1246817008e86ebf45cd3ea29322e95b07b5dd32
Nihalali <nihaluthman@hotmail.com>
2020-09-19 21:42:21 +0100

    Dog-photo/done

--- week-2/Homework/mandatory/3-dog-photo-gallery/style.js
+++ week-2/Homework/mandatory/3-dog-photo-gallery/style.js
@@ -1,8 +1,21 @@
+let newBut = document.getElementById('newBtn');
+let photogalary = document.getElementById('dogPhoto');
+
+newBu.addEventListener('click', function() {
+    let newPhoto = document.createElement('li');
+    let img = document.createElement('img');
+    newPhoto.appendChild(img);
+    photogalary.appendChild(newPhoto);
+   
+
+
 fetch(`https://dog.ceo/api/breeds/image/random`)
 .then(function(response) {
-return response.json;
+return (response.json());
 })
 
 .then(function(data){
+img.src = data.message;
+})
 
 })
