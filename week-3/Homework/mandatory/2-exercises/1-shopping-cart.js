e6393393ed59531e4a7bb74a0b3058f808cc8d98
Abdennour Hachemi <nouri89@hotmail.com>
2020-09-21 13:00:09 +0100

    solved

--- week-3/Homework/mandatory/2-exercises/1-shopping-cart.js
+++ week-3/Homework/mandatory/2-exercises/1-shopping-cart.js
@@ -1,20 +1,26 @@
 /*
-
 Complete the rest of this code to create an online shopping cart.
-
 The output of running your code should be:
-
     Your shopping cart has 3 items: Toilet Roll, Pasta, Eggs
-
     
 */
 
 class ShoppingCart {
-  // Add your code here
+	// Add your code here
 
-  cartContains() {
-    // Use console.log() to output everything contained in your cart
-  }
+	constructor() {
+		this.items = [];
+	}
+	addItem(item) {
+		this.items.push(item);
+	}
+
+	cartContains() {
+		// Use console.log() to output everything contained in your cart
+		console.log(
+			`Your shopping cart has ${this.items.length} items : ${this.items}.`
+		);
+	}
 }
 
 let myCart = new ShoppingCart(); // Creates an empty shopping cart
