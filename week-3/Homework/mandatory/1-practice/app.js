15a113a8f412acbacc344e29ad9fb78398908230
bireworld <mail2berhane@gmail.com>
2020-09-21 21:02:44 +0100

    weather app update

---
+++ week-3/Homework/mandatory/1-practice/app.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature-section")
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      //console.log(position)
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+
+      const api = `${proxy}
+      https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then(data => {
+          //console.log(data);
+          const { temperature, summary, icon } = data.currently;
+          // set DOM elements from Api
+
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+
+          //set Icons
+          setIcons(icon, document.querySelector(".icon"));
+        });
+    });
+  }
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
