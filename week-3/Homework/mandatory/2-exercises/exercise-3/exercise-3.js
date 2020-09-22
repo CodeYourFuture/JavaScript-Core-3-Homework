9867f5ab32a1f2615e1026ab24a511b3a1b32d93
Abdennour Hachemi <nouri89@hotmail.com>
2020-09-22 14:06:56 +0100

    Solved

--- week-3/Homework/mandatory/2-exercises/exercise-3/exercise-3.js
+++ week-3/Homework/mandatory/2-exercises/exercise-3/exercise-3.js
@@ -7,3 +7,38 @@
      { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
  ]
  
+ let order = [
+	{ itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
+	{ itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
+	{ itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
+	{ itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
+	{ itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+	{ itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+];
+
+let total = 0;
+console.log("QTY	ITEM			TOTAL");
+order.forEach((element) => {
+	console.log(
+		element.quantity +
+			"	" +
+			element.itemName +
+			"		" +
+			element.quantity * element.unitPrice
+	);
+	total += element.quantity * element.unitPrice;
+});
+console.log(" Total: " + total);
+
+/*
+function printReceipt(order) {
+	let oneOrderKeys = Object.keys(order[0]);
+	let orderKeys = oneOrderKeys.map(changeCase);
+
+	console.log(orderKeys);
+}
+function changeCase(word) {
+	return word.toUpperCase();
+}
+printReceipt(order);
+*/
