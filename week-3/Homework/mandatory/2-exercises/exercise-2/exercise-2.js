0e5cf94dd31f747fa44d88c76786aee74b3c8c24
Abdennour Hachemi <nouri89@hotmail.com>
2020-09-22 11:46:27 +0100

    solve task1

--- week-3/Homework/mandatory/2-exercises/exercise-2/exercise-2.js
+++ week-3/Homework/mandatory/2-exercises/exercise-2/exercise-2.js
@@ -9,3 +9,15 @@ let hogwarts = [
     { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
     { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
 ]
+
+let GryffindorPeople = hogwarts.filter((element) => {
+	if (element.house === "Gryffindor") {
+		return true;
+	}
+});
+
+let [{ firstName, lastName }] = GryffindorPeople;
+
+GryffindorPeople.forEach((element) => {
+	console.log(element.firstName + " " + element.lastName);
+});
