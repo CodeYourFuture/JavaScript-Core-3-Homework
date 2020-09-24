f2b255cbd51d0adae51a4fe33bbaf889e2d75867
Ali Haider <alihaiderx736@gmail.com>
2020-09-24 22:09:44 +0100

    exercise-1.js completed

--- week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
+++ week-3/Homework/mandatory/2-exercises/exercise-1/exercise-1.js
@@ -4,7 +4,7 @@ const personOne = {
     favouriteFood: 'Spinach'
 }
 
-function introduceYourself(___________________________) {
+function introduceYourself({name,age,favouriteFood}) {
     console.log (`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`); 
 }
 
